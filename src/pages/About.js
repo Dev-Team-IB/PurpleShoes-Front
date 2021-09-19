428b53590408cf6374f327eedce2c4a7116b3d9b
judemin <judemin2087@naver.com>
2021-09-19 17:15:04 +0900

    add : add heroku test code

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>About</h2>
+        <div className="App">
+            <button type="button" onClick={() => {
+                fetch('https://purple-shoes.herokuapp.com/test')
+                    .then((res) => {
+                        return res.json();
+                    })
+                    .then((data) => {
+                        console.log(data);
+                        alert(data.message);
+                    });
+            }}>get data</button>
         </div>
     );
 };
